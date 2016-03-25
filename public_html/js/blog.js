b352893fca9457bf6ba7cfbf1e534d94685b5292
caitlinlopez <CaitlinL128@hotmail.com>
2016-03-25 08:28:06 -0700

    video 9

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,3 +1,15 @@
-
+$(function (){
+   var APPLICATION_ID = "5A440563-A0F5-12D8-FFA2-E5CE3907D400",
+       SECRET_KEY = "D3E79385-74E2-3838-FF6D-0B7EA9CCFC00",
+       VERSION = "v1"
+       
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);   
+    
+    var user = new Backendless.user();
+    user.email = "caitlinlopez28@gmail.com";
+    user.password = "";
+    Backendless.userService.register(user);
+    
+});
 
 
